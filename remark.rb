fac2e0439d288ed68c619816bc0a71e17aa07e68
kota777 <kota777jp@icloud.com>
2016-01-31 20:04:37 +0900

    対応したanswerのカラムを取り出すところまで

--- remark.rb
+++ remark.rb
@@ -11,35 +11,46 @@ alphabet = 'a'
 
 client = Mysql2::Client.new(:host => 'localhost', :username => 'root', :password => 'root', :database => 'c_training4')
 client.query("SELECT * FROM progress INNER JOIN users ON progress.user_id = users.id WHERE test_id = #{test_id} ORDER BY test_id ASC, user_id ASC").each do |result| # 1人(1テスト)分の解答履歴
+  p result['name']
   used_text = Array.new
-  user_answer = Array.new
   collect_answer = Array.new
 
-  # p result['user_id']
   user_answer = result['list_selecter'].split(',') # ユーザの解答
-  puts 'user_answer:'
-  p user_answer
-  p user_answer.length
-  # p result['texts'] # 問題に使われた変数
 
   result['texts'].split(',').each {|text|
     used_text.push(alphabet_to_int_hash[text]) #問題に使われた変数を数字に変換
   }
-  puts 'used_text:'
-  p used_text
-  p used_text.length
 
-  result['list'].split(',').each do |question_id| # 問題のidでeach
+  question_ids = result['list'].split(',')
+  question_ids.each do |question_id| # 問題のidでeach
     client.query("SELECT * FROM questions WHERE id = #{question_id}").each do |question| # idから問題を抽出
       break if used_text.empty? # ユーザが答えたところまで
       collect_answer.push(question['answer'].split(',')[used_text.shift]) # 使われた変数の本当の答え
     end
   end
-  puts 'collect_answer:'
-  p collect_answer
-  p collect_answer.length
 
-  
+  begin
+    user_answer.length.times {|i|
+      client.query("SELECT * FROM answers WHERE user_id = #{result['user_id']} AND question_id = #{question_ids[i]} AND test_flag = 1").each do |question|
+        p question['id']
+      end
+      p user_answer[i] == collect_answer[i]
+    }
+  rescue => ex
+    puts ex.message
+    p result['user_id']
+
+    puts 'used_text:'
+    p result['texts'] # 問題に使われた変数
+
+    puts 'user_answer:'
+    p user_answer
+    p user_answer.length
+
+    puts 'collect_answer:'
+    p collect_answer
+    p collect_answer.length
+  end
 
   puts ''
 end
